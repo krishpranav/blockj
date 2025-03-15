06d8f70f7aa75a2face156358f5c9ce280d30d39
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 11:25:12 +0530

    blockj: agent[Block]

--- src/main/java/com/block/blockj/agent/Block.java
+++ src/main/java/com/block/blockj/agent/Block.java
@@ -40,4 +40,64 @@ public class Block implements Serializable {
 
         return index == block.index && timestamp.equals(block.timestamp);
     }
+
+    @Override
+    public int hashCode() {
+        int result = index;
+        result = 31 * result + timestamp.hashCode();
+        result = 31 * result + hash.hashCode();
+        result = 31 * result + previousHash.hashCode();
+        result = 31 * result + creator.hashCode();
+
+        return result;
+    }
+
+    public Block(int index, String prevHash, String creator) {
+        this.index = index;
+        this.previousHash = prevHash;
+        this.creator = creator;
+        timestamp = System.currentTimeMillis();
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public String calculateHash(String text) {
+        MessageDigest digest;
+
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+
+        } catch (NoSuchAlgorithmException e ) {
+            return "HASH_ERROR";
+        }
+
+        final byte bytes[] = digest.digest(text.getBytes());
+        final StringBuilder hexString = new StringBuilder();
+
+        for (final byte b : bytes) {
+            String hex = Integer.toHexString(0xff & b);
+
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
 }
