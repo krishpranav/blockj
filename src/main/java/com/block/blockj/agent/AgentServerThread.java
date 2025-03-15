9fb6d892da41f1c357b84d84990011a0f75ef628
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2025-03-15 11:39:59 +0530

    blockj: agent[AgentServerThread]

--- src/main/java/com/block/blockj/agent/AgentServerThread.java
+++ src/main/java/com/block/blockj/agent/AgentServerThread.java
@@ -1,9 +1,25 @@
 package com.block.blockj.agent;
 
+import java.io.IOException;
 import java.net.Socket;
 
 public class AgentServerThread {
 
     private Socket client;
     private final Agent agent;
+
+    AgentServerThread(final Agent agent, final Socket client) {
+        super(agent.getName() + System.currentTimeMillis());
+        this.agent = agent;
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        try {
+
+        } catch (ClassNotFoundException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
